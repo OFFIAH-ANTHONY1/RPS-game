cc95bb233c41d5676bf465505e651cac22be9eec
OFFIAH ANTHONY1 <ebubeoffiah95@gmail.com>
2024-07-07 00:51:59 +0100

    RPS-game

---
+++ Game.js
@@ -0,0 +1,21 @@
+function getRandomComputerResult() {
+    const options = ["Rock", "Paper", "Scissors"];
+    const randomIndex = Math.floor(Math.random() * options.length);
+    return options[randomIndex];
+  }
+  function hasPlayerWonTheRound(player, computer) {
+    let player;
+    let computer;
+    if (player == "Rock" && computer == "Scissors"){
+  return true;
+    }else if(player == "Scissors" && computer == "Paper"){
+      return true;
+    }else if(player == "Paper" && computer == "Rock"){
+      return true;
+    }else{
+      return false;
+    }
+  }
+  
+  console.log(hasPlayerWonTheRound("Rock", "Scissors")); 
+  console.log(hasPlayerWonTheRound("Scissors", "Rock")); 
